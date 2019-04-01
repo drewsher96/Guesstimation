5bffde341e9d10967fee4f61af915185d2f9a968
Sher <drew.sher@okstate.edu>
2019-04-01 18:51:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/amsher/guesstimation/ResultsPage.java
@@ -0,0 +1,13 @@
+package com.example.amsher.guesstimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_page);
+    }
+}
