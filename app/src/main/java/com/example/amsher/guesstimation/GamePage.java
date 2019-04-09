5ffecdbe8e1c8cfb02d4057bf008733b658535a9
ghugabo <gage.hugaboom@okstate.edu>
2019-04-09 17:30:09 -0500

    Working on letting us do ready checks for each user.

--- app/src/main/java/com/example/amsher/guesstimation/GamePage.java
+++ app/src/main/java/com/example/amsher/guesstimation/GamePage.java
@@ -3,11 +3,18 @@ package com.example.amsher.guesstimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class GamePage extends AppCompatActivity {
+    public Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://guesstimation-445f5.firebaseio.com/Count");
+
     }
 }
--- app/src/main/java/com/example/amsher/guesstimation/IntroPage.java
+++ app/src/main/java/com/example/amsher/guesstimation/IntroPage.java
@@ -3,39 +3,72 @@ package com.example.amsher.guesstimation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IntroPage extends AppCompatActivity {
 
-    public EditText input;
+    private Button mButton;
+    private EditText userID;
+    private EditText gameID;
+
+    public Firebase mRef;
+    public Firebase mRefInstance;
+    public Firebase mRefInstanceUser;
+    public Firebase mRefInstanceReady;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_page);
+
+
+        mButton = (Button) findViewById(R.id.button);
+        userID = (EditText) findViewById(R.id.editText1);
+        gameID = (EditText) findViewById(R.id.editText2);
+
     }
 
     protected void onHostClick (View v) {
-        input = (EditText) findViewById(R.id.editText1);
-        String host = input.getText().toString();
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://guesstimation-445f5.firebaseio.com/Game");
 
-        Intent intent = new Intent(getApplicationContext(),AdminPage.class);
-        startActivity(intent);
+        String uValue = userID.getText().toString();
+        String gValue = gameID.getText().toString();
 
+        mRefInstance = mRef.child(gValue);
+        mRefInstanceUser = mRefInstance.child("Host");
+        mRefInstanceUser.setValue(uValue);
 
+        Intent intent1 = new Intent(getApplicationContext(), AdminPage.class);
+        startActivity(intent1);
     }
 
     protected void onJoinClick (View v) {
-        input = (EditText) findViewById(R.id.editText2);
-        String join = input.getText().toString();
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://guesstimation-445f5.firebaseio.com/Game");
 
-        Intent intent = new Intent(getApplicationContext(),JoinPage.class);
-        startActivity(intent);
+        String uValue = userID.getText().toString();
+        String gValue = gameID.getText().toString();
+        int score = 0;
+        boolean ready = false;
 
+        mRefInstance = mRef.child(gValue).child(uValue);
 
+        Intent intent1 = new Intent(getApplicationContext(), GamePage.class);
+        startActivity(intent1);
     }
 }
