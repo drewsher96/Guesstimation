a0a3242407cd7c2cd8ad730e4b33031e1990b43f
Gage <gage.hugaboom@okstate.edu>
2019-04-11 16:25:15 -0500

    Changed around the results and admin pages.

--- app/src/main/java/com/example/amsher/guesstimation/ResultsPage.java
+++ app/src/main/java/com/example/amsher/guesstimation/ResultsPage.java
@@ -3,7 +3,18 @@ package com.example.amsher.guesstimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class ResultsPage extends AppCompatActivity {
+
+    public Firebase mRef;
+    public Firebase mRefInstance;
+    public Firebase mRefInstanceUser1;
+    public Firebase mRefInstanceUser2;
+    public Firebase mRefInstanceUser3;
+    public Firebase mRefInstanceScore1;
+    public Firebase mRefInstanceScore2;
+    public Firebase mRefInstanceScore3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
